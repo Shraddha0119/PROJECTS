9ccab8643ddbd31528f4b08c55913545e3c356e2
Shraddha0119 <gunjkarshraddha6@gmail.com>
2026-05-25 18:02:17 +0530

    the project is completed

--- MERN-project/FRONTEND/src/components/About.jsx
+++ MERN-project/FRONTEND/src/components/About.jsx
@@ -13,7 +13,13 @@ const About = () => {
                     <p>the only thing we're serious about is food</p>
                 </div>
                 <p className='mid'>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, saepe minima repudiandae ratione id laborum modi quibusdam incidunt, fuga beatae eos commodi, sint ipsa molestiae libero nulla animi molestias cupiditate.
+                   Welcome to our hotel, where delicious food meets unforgettable experiences. 
+                   From rich flavors and freshly prepared dishes to a warm and elegant dining atmosphere, 
+                   we serve meals that satisfy every craving. Our chefs carefully craft every dish using 
+                   high-quality ingredients, bringing you the perfect blend of taste, freshness, and passion. 
+                   Whether you are enjoying a family dinner, a romantic evening, or a quick meal with friends, 
+                   our restaurant promises exceptional service and mouthwatering flavors in every bite. 
+                   Come and experience the joy of great food, comfort, and hospitality all in one place.
                 </p>
                 <Link to={"/"}> Explore Menu{""}
                     <span>
--- MERN-project/FRONTEND/src/components/Menu.jsx
+++ MERN-project/FRONTEND/src/components/Menu.jsx
@@ -7,7 +7,7 @@ const Menu = () => {
         <div className="container">
             <div className="heading_section">
                 <h1 className='heading'>POPULAR DISHES</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam pariatur iusto ipsam adipisci iste corporis reiciendis laboriosam possimus, officiis quasi fuga repellendus repudiandae! Distinctio nihil impedit repellendus est sed numquam.</p>
+                <p>Our dishes are prepared with fresh ingredients, rich flavors, and a perfect blend of spices to create an unforgettable dining experience. Every meal is carefully crafted with passion, offering delicious taste, beautiful presentation, and satisfying quality in every bite. From comforting meals to flavorful specialties, our food brings warmth, freshness, and happiness to your table..</p>
             </div>
             <div className="dishes_container">
                 {
